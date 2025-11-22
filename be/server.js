5f97fc82b1fe2ef90b15b094adca7ec59e5bc700
Hoang Phong <hoangphong7889@gmail.com>
2025-11-23 00:01:27 +0700

    + BE

--- be/server.js
+++ be/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Bật CORS cho mọi domain
+app.use(cors());
 
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Xin chào từ backend!", time: new Date() });
